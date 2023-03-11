071b86ce1fc8c9a93a8f53a5a1b7864b4c7afe48
imjho <jholokar@wesleyan.edu>
2023-03-11 09:14:17 -0800

    add har table
    Co-authored-by: IuliiaSam <iuliia.samokhvalova@gmail.com>

--- src/devtools.js
+++ src/devtools.js
@@ -1,69 +1,93 @@
-// write frontend logic here
-
-// write Chrome extension logic
-// chrome.devtools.panels.create();
-
-// chrome.devtools.network.onNavigated.addListener(url => {});
-// chrome.devtools.network.onRequestFinished.addListener(request => {
-//   // add Nextwork logic here?
-//   // send data to frontend
-// });
-
 let youClickedOn;
+let dataContainer;
+let harTable;
 
-chrome.devtools.panels.create("NextWork", "logo.jpeg", "panel.html", panel => {
+chrome.devtools.panels.create('NextWork', 'logo.jpeg', './src/panel.html', (panel) => {
   // code invoked on panel creation
-  panel.onShown.addListener(extPanelWindow => {
-    let sayHello = extPanelWindow.document.querySelector("#sayHello");
-    youClickedOn = extPanelWindow.document.querySelector("#youClickedOn");
-    sayHello.addEventListener("click", () => {
+  panel.onShown.addListener((extPanelWindow) => {
+    let sayHello = extPanelWindow.document.querySelector('#sayHello');
+    youClickedOn = extPanelWindow.document.querySelector('#youClickedOn');
+    dataContainer = extPanelWindow.document.querySelector('#data');
+    harTable = extPanelWindow.document.querySelector('.har-table');
+    sayHello.addEventListener('click', () => {
       // show a greeting alert in the inspected page
-      chrome.devtools.inspectedWindow.eval(
-        'alert("Hello from the NextWork Extension");'
-      );
+      chrome.devtools.inspectedWindow.eval('alert("Hello from the NextWork Extension");');
     });
   });
 });
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+const appendHar = (data) => {
+  console.log('harTable', harTable);
+  let harTableHead = harTable ? harTable.querySelector('thead') : null;
+
+  if (!harTable) {
+    harTable = document.createElement('table');
+    harTable.classList.add('har-table');
+    harTableHead = document.createElement('thead');
+    const headRow = document.createElement('tr');
+
+    const urlHeader = document.createElement('th');
+    urlHeader.innerText = 'URL';
+    const methodHeader = document.createElement('th');
+    methodHeader.innerText = 'Method';
+    const statusHeader = document.createElement('th');
+    statusHeader.innerText = 'Status';
+    const sizeHeader = document.createElement('th');
+    sizeHeader.innerText = 'Size';
+
+    headRow.append(urlHeader, methodHeader, statusHeader, sizeHeader);
+    harTableHead.append(headRow);
+    harTable.append(harTableHead);
+  }
+
+  let row = document.createElement('tr');
+  let url = document.createElement('td');
+  url.innerText = data.request.url;
+  let method = document.createElement('td');
+  method.innerText = data.request.method;
+  let status = document.createElement('td');
+  status.innerText = data.response.status;
+  let size = document.createElement('td');
+  size.innerText = data.response.bodySize;
+
+  row.append(url, method, status, size);
+  harTableHead.append(row);
+
+  return harTable;
+};
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  // if (message.data) {
+  //   console.log('Received message from service worker:', message.data);
+  //   // dataContainer.innerHTML = `${message.data}`;
+
+  if (message.data) {
+    console.log('Received message from service worker:', message.data);
+
+    dataContainer.append(appendHar(JSON.parse(message.data)));
+  }
+
   // Messages from content scripts should have sender.tab set
-  console.log("request.type", request.type);
-  if (sender.tab && request.click == true) {
+  if (sender.tab && message.click == true) {
     if (youClickedOn) {
-      youClickedOn.innerHTML = `You clicked on position (${request.xPosition}, ${request.yPosition}) in the inspected page.`;
+      // console.log("panels:", chrome.devtools.panels);
+      youClickedOn.innerHTML = `You clicked on position (${message.xPosition}, ${message.yPosition}) in the inspected page.`;
     }
     sendResponse({
-      xPosition: request.xPosition,
-      yPosition: request.yPosition,
+      xPosition: message.xPosition,
+      yPosition: message.yPosition,
     });
   }
 });
 
 // Create a connection to the background service worker
 const backgroundPageConnection = chrome.runtime.connect({
-  name: "devtools-page",
+  name: 'devtools-page',
 });
 
 // Relay the tab ID to the background service worker
+console.log('about to post message from devtools.js');
 backgroundPageConnection.postMessage({
-  name: "init",
+  name: 'init',
   tabId: chrome.devtools.inspectedWindow.tabId,
 });
-
-// var source = new EventSource("http://localhost:3001/stream");
-// source.addEventListener(
-//   "open",
-//   function (e) {
-//     document.getElementById("content").innerHTML +=
-//       "Connections to the server established..<br/>";
-//   },
-//   false
-// );
-// source.onmessage = function (e) {
-//   document.getElementById("content").innerHTML += e.data + "<br/>";
-// };
-// document.getElementById("stopButton").onclick = function () {
-//   document.getElementById("content").innerHTML +=
-//     "Listening to server events stopped..<br/>";
-//   source.close();
-// };
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,21 +1,15 @@
-// logic to make sure that the correct panel data is served to its respective tab
-
-// attach, store, update frontend props if tab is refreshed
-
-// remove tab-related information if tab is closed
-
 let id = null;
 const connections = {};
 
-chrome.runtime.onConnect.addListener(devToolsConnection => {
+chrome.runtime.onConnect.addListener((devToolsConnection) => {
   // Assign the listener function to a variable so we can remove it later
   let devToolsListener = (message, sender, sendResponse) => {
-    if (message.name == "init") {
+    if (message.name == 'init') {
       id = message.tabId;
-      console.log("id", id);
       connections[id] = devToolsConnection;
       // Send a message back to DevTools
-      connections[id].postMessage("Connected!");
+      console.log('one line above connections[id].postMessage("Connected!")');
+      connections[id].postMessage('Connected!');
     }
   };
 
@@ -26,3 +20,29 @@ chrome.runtime.onConnect.addListener(devToolsConnection => {
     devToolsConnection.onMessage.removeListener(devToolsListener);
   });
 });
+
+var source = new EventSource('http://localhost:3001/stream');
+source.addEventListener(
+  'open',
+  function (e) {
+    // send the information to the panel
+    connections[id].postMessage({
+      name: 'init',
+      tabId: id,
+    });
+    console.log('Connection to the server established');
+  },
+  false
+);
+
+source.onmessage = function (e) {
+  console.log('Received message from server: ', e.data);
+  // send the information to the panel
+  connections[id].postMessage({
+    name: 'init',
+    tabId: id,
+  });
+
+  chrome.runtime.sendMessage({ data: e.data, log: e.log });
+  // document.getElementById("content").innerHTML += e.data + "<br/>";
+};
