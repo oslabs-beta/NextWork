3bfbb0ca33902673130d2b57aa7e7304f0c0b7e2
Anne Blazejack <annieblazejack@gmail.com>
2023-04-11 22:17:14 -0400

    re-added README, updated postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 const packageJsonPath = path.resolve(process.cwd(), 'package.json');
-const packageJson = require(packageJsonPath);
+import packageJson from packageJsonPath;
 
 packageJson.scripts = packageJson.scripts || {};
 packageJson.scripts['nextWorkDev'] =
-  'node --require ./nextWork/nextWork.js ./node_modules/.bin/next dev';
+  'node --require ./node_modules/next-work/lib/nextWork.js ./node_modules/.bin/next dev';
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
