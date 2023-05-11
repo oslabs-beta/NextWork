f8340147ccc0d8f99dcfd24ba1fd5edb2c645b2c
imjho <jholokar@wesleyan.edu>
2023-05-10 18:49:15 -0700

    add state management on chrome storage

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,19 +1,33 @@
-//import all components
-import FilterBar from "./components/FilterBar";
-import Table from "./components/Table";
-import Footer from "./components/Footer";
-import { useState, useEffect } from "react";
+import FilterBar from './components/FilterBar';
+import Table from './components/Table';
+import Footer from './components/Footer';
+import { useState, useEffect } from 'react';
 
 export default function App() {
   const [entries, setEntries] = useState([]);
-  const [filterInput, setFilterInput] = useState("");
+  const [filterInput, setFilterInput] = useState('');
 
   const [filteredEntries, setFilteredEntries] = useState([]);
 
+  const saveToStorage = (entry) => {
+    chrome.storage.local.get(['entries'], (result) => {
+      let updatedEntries = result.entries ? [...result.entries, entry] : [entry];
+      chrome.storage.local.set({ entries: updatedEntries });
+    });
+  };
+
+  const loadFromStorage = () => {
+    chrome.storage.local.get(['entries', 'filterInput'], (result) => {
+      if (result.entries) {
+        setEntries(result.entries);
+      }
+      if (result.filterInput) {
+        setFilterInput(result.filterInput);
+      }
+    });
+  };
+
   const handleInput = (filterInput) => {
-    //use array.filter?
-    //check through each entry in array to see if it contains the string
-    //setFilteredEntries to be only the subset of entries that contains the string
     console.log(filterInput);
     setFilterInput(filterInput);
   };
@@ -21,26 +35,26 @@ export default function App() {
   useEffect(() => {
     chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       if (message.data) {
-        console.log(
-          "Received message from service worker in App.tsx:  ",
-          message.data
-        );
-        // entries.push(JSON.parse(message.data));
-        setEntries([...entries, JSON.parse(message.data)]);
+        const newEntry = JSON.parse(message.data);
+        setEntries((prevEntries) => [...prevEntries, newEntry]);
+        saveToStorage(newEntry);
       }
     });
-  });
+  }, []);
 
   useEffect(() => {
     setFilteredEntries(
       entries.filter((entry) =>
-        //helper function to take entry, extract relevant fields, return array. That way we can search by everything, not just URL
         JSON.stringify(Object.values(entry.request))
           .toLowerCase()
           .includes(filterInput.toLowerCase())
       )
     );
   }, [entries, filterInput]);
+
+  useEffect(() => {
+    loadFromStorage();
+  }, []);
 
   return (
     <div>
